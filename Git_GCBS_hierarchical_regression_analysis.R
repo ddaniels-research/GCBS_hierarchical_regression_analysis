97f459e6259a02a45289029e77b69eef0061297f
Drew Daniels <dsdaniels92@gmail.com>
2026-03-07 15:49:51 -0500

    Initial commit: Project title and purpose.

---
+++ Git_GCBS_hierarchical_regression_analysis.R
@@ -0,0 +1,8 @@
+# Psychological and Demographic Antecedents of Conspiracy Beliefs:
+# A Hierarchical Regression Analysis
+
+# Project:    Generic Conspiracist Beliefs Scale (GCBS) Regression Analysis
+# Researcher: Drew Daniels
+# Purpose:    Hierarchical regression examining personality, cognitive, and 
+#             demographic predictors of conspiracy beliefs, with focus on
+#             suppression effects and relative importance analysis
